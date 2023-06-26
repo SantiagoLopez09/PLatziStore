83146f014d63ebcd2a1ae8ad72b26251ad448b82
Santiago Lopez <santiagolopeza30@gmail.com>
2023-06-26 16:27:50 -0500

    Google Analytics con Next.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,4 @@
-const { Main, NextScript } = require("next/document")
-const { default: Head } = require("next/head")
-
-import Document { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document{
   render(){
@@ -9,12 +6,21 @@ class MyDocument extends Document{
       <Html lang="en">
         <Head>
           <title>React Shop</title>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6ZT99DZ0HY"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6ZT99DZ0HY');`
+          }} />
           <body>
             <Main />
-            <NextScript />           
+            <NextScript />
           </body>
         </Head>
       </Html>
-    )
+    );
   }
-}
+};
+
+export default MyDocument;
