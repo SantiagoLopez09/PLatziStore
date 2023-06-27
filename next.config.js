cb1720e075345494a8d77478b7a87ce6f016fc45
Santiago Lopez <santiagolopeza30@gmail.com>
2023-06-27 16:14:15 -0500

    Progressive Web Apps con Next.js 12

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
   images: {
     domains: ['placeimg.com', 'picsum.photos','placeing.com', 'api.escuelajs.co', 'fastly.picsum.photos', 'cdn.pixabay.com'],
   },
-  reactStrictMode: true,
-  // env: {
-  //   customKey: 'customValue',
-  // },
-  // basePath: "/dist",
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: '/hello',
-  //       permanent: true,
-  //     },
-  //   ]
-  // }
-};
+});
 
-module.exports = nextConfig;
